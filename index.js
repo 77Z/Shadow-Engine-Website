b221017dbff28a305bc63c78aab97ec9ac9cbe95
Vince Richter <r2d2fan20@outlook.com>
2020-11-01 12:25:23 -0600

    Wee Website

---
+++ index.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    document.getElementById("dlbtn").addEventListener("click", function() {
+        window.open("https://github.com/77Z/Shadow-Engine/releases");
+    });
+    document.getElementById("ghbtn").addEventListener("click", function() {
+        window.open("https://github.com/77Z/Shadow-Engine");
+    });
+};
